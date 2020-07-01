0555f0b910684bf740a5fb064a4dd9af52c0e984
Payton Burr <36023709+payton-burr@users.noreply.github.com>
2020-07-01 16:11:31 -0600

    JS/CSS nav burger now works

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let controller;
 let slideScene;
 let pageScene;
-let mouse = document.querySelector('.cursor');
-let mouseText = mouse.querySelector('span');
+const mouse = document.querySelector('.cursor');
+const mouseText = mouse.querySelector('span');
+const burger = document.querySelector('.burger');
 
 function animateSlides() {
   controller = new ScrollMagic.Controller();
@@ -79,6 +80,25 @@ function activeCursor(e) {
   }
 }
 
+function navToggle(e) {
+  if (!e.target.classList.contains('active')) {
+    e.target.classList.add('active');
+    gsap.to('.line1', 0.5, { rotate: '45', y: 5, background: 'black' });
+    gsap.to('.line2', 0.5, { rotate: '-45', y: -5, background: 'black' });
+    gsap.to('#logo', 1, { color: 'black' });
+    gsap.to('.nav-bar', 1, { clipPath: 'circle(2500px at 100% -10%)' });
+    document.body.classList.add('hide');
+  } else {
+    e.target.classList.remove('active');
+    gsap.to('.line1', 0.5, { rotate: '0', y: 0, background: 'white' });
+    gsap.to('.line2', 0.5, { rotate: '0', y: 0, background: 'white' });
+    gsap.to('#logo', 1, { color: 'white' });
+    gsap.to('.nav-bar', 1, { clipPath: 'circle(50px at 100% -10%)' });
+    document.body.classList.remove('hide');
+  }
+}
+
+burger.addEventListener('click', navToggle);
 window.addEventListener('mousemove', cursor);
 window.addEventListener('mouseover', activeCursor);
 
